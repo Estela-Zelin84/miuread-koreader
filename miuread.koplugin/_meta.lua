7c3c5eb852922bf4d5f2537fe91585a21c010ea3
miumiupy98-art <miumiupy98@gmail.com>
2026-07-29 21:42:06 +0900

    beta

--- miuread.koplugin/_meta.lua
+++ miuread.koplugin/_meta.lua
@@ -2,5 +2,5 @@ local Text = require("miuread.text")
 return {
     fullname = "觅阅 · 微信读书助手",
     description = Text.tr("Independent WeRead client for KOReader."),
-    version = "2.3.4-beta.5",
+    version = "2.5.0-beta.1",
 }
